46c4df997eb3239880eda7113845b5ed84bc051c
MaeDaeryn <c.m.berbig@gmail.com>
2022-11-12 12:07:46 +0100

    DialogueSystem + Audio

---
+++ 2DBeansJamNov2022/Assets/Scripts/DialogueSystem/NPC.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+
+public class NPC : MonoBehaviour
+{
+
+    public Dialogue dialogue;
+
+    public void TriggerDialogue()
+    {
+        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+    }
+
+}
